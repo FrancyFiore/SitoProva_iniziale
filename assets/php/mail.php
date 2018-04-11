cf222372d40148c418ef68c295f17d44519787f1
FrancyFiore <francesco.fiorelli@studenti.unitn.it>
2018-04-11 19:20:20 +0200

    form

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,11 +1,11 @@
 <?php
-$name = $_POST['nome'];
-$city = $_POST['città'];
-$phone = $_POST['telefono'];
+$nome = $_POST['nome'];
+$città = $_POST['città'];
+$telefono = $_POST['telefono'];
 $email = $_POST['email'];
-$message = $_POST['messaggio'];
-$formcontent=" From: $name \n Città: $city \n Phone: $phone \n Email: $email \n Message: $message";
-$recipient = "YOUREMAIL@HERE.COM";
+$messaggio = $_POST['messaggio'];
+$formcontent=" From: $nome \n Città: $città \n Phone: $telefono \n Email: $email \n Message: $messaggio";
+$recipient = "iperfrancy@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
