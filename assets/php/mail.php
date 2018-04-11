04e8a6a1b5795bfcba043623b9b3c7a6581ed056
FrancyFiore <francesco.fiorelli@studenti.unitn.it>
2018-04-11 19:23:04 +0200

    Add files via upload

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,13 +1,10 @@
 <?php
-$nome = $_POST['nome'];
-$citta = $_POST['citta'];
-$telefono = $_POST['telefono'];
-$email = $_POST['email'];
-$messaggio = $_POST['messaggio'];
-$formcontent=" From: $nome \n Città: $citta \n Phone: $telefono \n Email: $email \n Message: $messaggio";
-$recipient = "iperfrancy@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+$to      = 'iperfrancy@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = array(
+    'From' => 'webmaster@example.com',
+);
+
+mail($to, $subject, $message, $headers);
 ?>
