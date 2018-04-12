82901ce57651fe8c0057d75e7e179fcdbe4ff534
FrancyFiore <francesco.fiorelli@studenti.unitn.it>
2018-04-12 09:41:12 +0200

    aggiornamento

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,10 +1,13 @@
 <?php
-$to      = 'iperfrancy@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = array(
-    'From' => 'webmaster@example.com',
-);
-
-mail($to, $subject, $message, $headers);
+$nome = $_POST['nome'];
+$citta = $_POST['citta'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+$messaggio = $_POST['messaggio'];
+$formcontent=" From: $nome \n Città: $citta \n Phone: $telefono \n Email: $email \n Message: $messaggio";
+$recipient = "iperfrancy@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
